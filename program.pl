bbb45abf560a3d91666d18860b0f06138a12d5bc
gabelopes <gabriellopes.nunes@hotmail.com>
2021-09-26 14:17:01 -0300

    Fixes entry point Program for proper compilation

--- program.pl
+++ program.pl
@@ -1,14 +1,28 @@
 start :-
-  current_prolog_flag(argv, [ModuleName|_]),
-  load_module(ModuleName, Module),
-  Module:initialize, !,
-  Module:run,
+  current_prolog_flag(argv, [Module|_]),
+  get_module(Module, _, Initialize, Run),
+  call(Initialize), !,
+  call(Run),
   halt.
 start :-
   halt(2).
 
-load_module(ModuleName, Module) :-
-  atom_concat(ModuleName, '_cli', Module),
-  use_module(modules/ModuleName/Module).
-load_module(ModuleName, _) :-
-  format("Could not find module '~w'.", [ModuleName]), nl.
+include_modules :-
+  directory_files(modules, [_, _|Modules]),
+  maplist(include_module, Modules).
+
+include_module(Name) :-
+  get_module(Name, CLI, Initialize, Run),
+  use_module(modules/Name/CLI, [
+    initialize/0 as Initialize,
+    run/0 as Run
+  ]).
+include_module(Name, _) :-
+  format("Could not find module '~w'.", [Name]), nl.
+
+get_module(Name, CLI, Initialize, Run) :-
+  atom_concat(Name, '_cli', CLI),
+  atom_concat(Name, '_initialize', Initialize),
+  atom_concat(Name, '_run', Run).
+
+:- include_modules.
